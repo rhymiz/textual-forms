e1e197d590e7ad738452cd3722a48078f6e79236
Lemuel Boyce <lemuelboyce@gmail.com>
2022-11-30 23:33:51 -0500

    general clean

--- src/textual_forms/forms.py
+++ src/textual_forms/forms.py
@@ -1,19 +1,21 @@
-from __future__ import annotations
-
-from typing import Dict, List, Type
+from typing import Any, Dict, List, Type
 
 from textual.app import ComposeResult
 from textual.containers import Horizontal
 from textual.message import Message, MessageTarget
 from textual.reactive import Reactive
 from textual.widget import Widget
-from textual.widgets import Button, Static
+from textual.widgets import Button
 
 from .fields import FormField
 
 
 class Form(Widget):
-    DEFAULT_CSS = """  
+    """
+    A container widget for inputs and buttons that
+    behaves kind of like a web-based form with validations.
+    """
+    DEFAULT_CSS = """
     Form {
         padding: 1
     }
@@ -29,10 +31,10 @@ class Form(Widget):
     }
     """
 
-    data: Reactive[dict] = Reactive({})
+    data: Reactive[Dict[str, Any]] = Reactive({})
     valid: Reactive[bool] = Reactive(False)
-    button_widget: Type[Static] = Button
-    form_field_widget: Type[Widget] = FormField
+    button_widget: Type[Button] = Button
+    form_field_widget: Type[FormField] = FormField
     button_group_container: Type[Widget] = Horizontal
 
     class Event(Message):
@@ -40,15 +42,26 @@ class Form(Widget):
         message that's emitted whenever a button is pressed
         """
 
-        def __init__(self, sender: MessageTarget, event: str, data: dict) -> None:
+        def __init__(
+            self,
+            sender: MessageTarget,
+            event: str,
+            data: Dict[str, Any]
+        ) -> None:
             self.data = data
             self.event = event
             super().__init__(sender)
 
-    def __init__(self, *, fields: List[Dict], buttons: List[Dict], **kwargs):
+    def __init__(
+        self,
+        *,
+        fields: List[Dict[str, Any]],
+        buttons: List[Dict[str, Any]],
+        **kwargs: Dict[str, Any]
+    ) -> None:
         self.fields = fields
         self.buttons = buttons
-        self._watching_valid = []
+        self._watching_form_valid: List[Button] = []
         super().__init__(**kwargs)
 
     def compose(self) -> ComposeResult:
@@ -58,7 +71,7 @@ class Form(Widget):
                 data=field,
             )
 
-        buttons = []
+        buttons: List[Button] = []
         for button in self.buttons:
             btn = self.button_widget(
                 id=button['id'],
@@ -68,22 +81,31 @@ class Form(Widget):
                 disabled=button.get('disabled', True)
             )
             if button.get('watch_form_valid', False):
-                self._watching_valid.append(btn)
+                self._watching_form_valid.append(btn)
             buttons.append(btn)
 
         yield self.button_group_container(*buttons, id='button_group')
 
-    async def watch_valid(self, allow_submit: bool) -> None:
-        for button in self._watching_valid:
-            button.disabled = not allow_submit
+    async def watch_valid(self, valid: bool) -> None:
+        """
+        enable/disable buttons based on the state of the form
+        """
+        for button in self._watching_form_valid:
+            button.disabled = not valid
 
     async def on_form_field_changed(self, message: FormField.Changed) -> None:
-        self.data[message.sender.id] = message.value
+        """
+        listens for form field changes and assesses the validity of the form
+        """
+        self.data[getattr(message.sender, 'id')] = message.value
         self.valid = all([x.valid for x in self.query(FormField)])
 
     async def on_button_pressed(self, message: Button.Pressed) -> None:
+        """
+        emit an event whenever any button inside the form is pressed
+        """
         await self.emit(self.Event(
             self,
             data=self.data,
-            event=message.button.id,
+            event=getattr(message.button, 'id'),
         ))
--- src/textual_forms/validators.py
+++ src/textual_forms/validators.py
@@ -1,27 +1,44 @@
 import re
 from collections import defaultdict
-from typing import Tuple, Union
+from typing import Any, Tuple, Union, Dict
 
 _INTEGER = re.compile(r'\d+$')
 _NUMBERS = re.compile(r'\d*[.,]?\d+$')
 
 
 class FieldValidator:
-    def validate(self, value: str, rules: str, **kwargs) -> Tuple[bool, Union[str, None]]:
-        return True, None
+    """
+    base field validator class
+    """
 
-    def __call__(self, value: str, required: bool, **kwargs) -> Tuple[bool, str]:
+    def validate(
+        self,
+        value: str,
+        rules: Dict[str, Any]
+    ) -> Tuple[bool, Union[str, None]]:
+        """"""
+        raise NotImplementedError
+
+    def __call__(
+        self,
+        value: str,
+        required: bool,
+        rules: Dict[str, Any]
+    ) -> Tuple[bool, Union[str, None]]:
         if required and value is None:
             return False, 'This value is required'
-        return self.validate(value, required=required, **kwargs)
+        return self.validate(value, rules=rules)
 
 
 class IntegerFieldValidator(FieldValidator):
+    """a validator that ensures a given value is an integer.
+    additional, it can also ensure that the number is within certain bounds.
+    """
+
     def validate(
-            self,
-            value: str,
-            rules: dict,
-            **kwargs
+        self,
+        value: str,
+        rules: Dict[str, Any],
     ) -> Tuple[bool, Union[str, None]]:
         """
         Validates whether a given string is a valid integer.
@@ -33,34 +50,43 @@ class IntegerFieldValidator(FieldValidator):
             return False, 'Invalid integer'
 
         int_value = int(value)
-        min_number = rules.get('min')
-        max_number = rules.get('max')
+        min_number: Union[int, None] = rules.get('min')
+        max_number: Union[int, None] = rules.get('max')
 
-        message = None
-        is_valid = True
+        message: Union[str, None] = None
+        is_valid: bool = True
 
-        if min_number and not max_number:
+        if min_number and max_number:
+            is_valid = min_number <= int_value <= max_number
+            message = (
+                f"value must be between {min_number} and {max_number}"
+                if not is_valid
+                else None
+            )
+            return is_valid, message
+
+        if min_number:
             if int_value < min_number:
                 message = f'value must be greater than or equal to {min_number}'
                 is_valid = False
-        elif max_number and not min_number:
+        elif max_number:
             if int_value > max_number:
                 message = f'value must be less than or equal to {max_number}'
                 is_valid = False
-        elif not (min_number <= int_value <= max_number):
-            message = f'value must be between {min_number} and {max_number}'
-            is_valid = False
         return is_valid, message
 
 
 class StringValidator(FieldValidator):
-    def validate(
-            self,
-            value: str,
-            rules: dict,
-            **kwargs
-    ) -> Tuple[bool, Union[str, None]]:
+    """a validator that "ensures" a given value is an string.
+    additional, it can also ensure that the value is within certain bounds.
+    """
 
+    def validate(
+        self,
+        value: str,
+        rules: Dict[str, Any],
+    ) -> Tuple[bool, Union[str, None]]:
+        """validate a given string"""
         min_length = rules.get('min_length')
         max_length = rules.get('max_length')
 
@@ -68,30 +94,42 @@ class StringValidator(FieldValidator):
         is_valid = True
         string_length = len(value)
 
-        if min_length and not max_length:
+        if min_length and max_length:
+            is_valid = min_length <= string_length <= max_length
+            message = (
+                f"value must be between {min_length} and {max_length} characters"
+                if is_valid
+                else None
+            )
+            return is_valid, message
+
+        if min_length:
             if string_length < min_length:
                 message = f"value must not be less than {min_length} characters"
                 is_valid = False
-        elif max_length and not min_length:
+        elif max_length:
             if string_length > max_length:
                 message = f"value must be no longer than {max_length} characters"
-                is_valid = False
-        elif min_length and max_length:
-            if not (min_length <= string_length <= max_length):
-                message = f"value must be between {min_length} and {max_length} characters"
                 is_valid = False
         return is_valid, message
 
 
 class NumberFieldValidator(FieldValidator):
-    def validate(self, value, rules, **kwargs) -> Tuple[bool, Union[str, None]]:
+    """a validator that ensures a given value is numeric.
+    """
+
+    def validate(
+        self,
+        value: str,
+        rules: Dict[str, Any]
+    ) -> Tuple[bool, Union[str, None]]:
         match = bool(_NUMBERS.match(value))
         if not match:
             return False, "invalid number"
         return True, None
 
 
-validators = defaultdict(lambda: StringValidator())
+validators: Dict[str, FieldValidator] = defaultdict(lambda: StringValidator())
 validators.update(**{
     'number': NumberFieldValidator(),
     'integer': IntegerFieldValidator(),
